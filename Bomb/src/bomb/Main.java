56aad160f51d4761fa2cd43b2a36df7578aae7d9
joeyPark8 <61180930+joeyPark8@users.noreply.github.com>
2020-12-10 20:11:51 +0900

    Add files via upload

--- Bomb/src/bomb/Main.java
+++ Bomb/src/bomb/Main.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
